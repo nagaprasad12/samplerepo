0c2bd352d505c5998a8cdb32ede01e7afc41767c
nagaprasad GOLLU <nagaprasadGOLLU@MTLP-2268.local>
2023-01-05 18:00:03 +0530

    adding dart file

---
+++ sample.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SampleDemo extends StatefulWidget {
+  const SampleDemo({Key? key}) : super(key: key);
+
+  @override
+  State<SampleDemo> createState() => _SampleDemoState();
+}
+
+class _SampleDemoState extends State<SampleDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
